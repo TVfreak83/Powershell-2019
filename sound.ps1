231574ab80cba7d2d547ce9f06703a6db1014cc5
tvfreak83 <Administrator@LOCAL.FREAK.TM>
2019-09-12 16:15:37 +0200

    end

---
+++ sound.ps1
@@ -0,0 +1,25 @@
+﻿Function Set-SpeakerVolume
+
+{ Param (
+
+  [switch]$min,
+
+  [switch]$max)
+
+ 
+
+  $wshShell = new-object -com wscript.shell
+
+ 
+
+  If ($min)
+
+  {1..50 | % {$wshShell.SendKeys([char]174)}}
+
+  ElseIf ($max)
+
+  {1..50 | % {$wshShell.SendKeys([char]175)}}
+
+  Else
+
+  {$wshShell.SendKeys([char]173)} }
---
+++ speaker.ps1
@@ -0,0 +1,3 @@
+﻿Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.Speak('To bad it was the last lesson, just when it started to get in to our brain  POWERSHELL forever')
